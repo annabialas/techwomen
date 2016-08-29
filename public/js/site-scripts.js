515c47bf043563f9254b6ac0fbe4fd52482e3c73
annabialas <ab5086@nyu.edu>
2016-08-29 16:34:14 -0400

    adds functional modals

--- public/js/site-scripts.js
+++ public/js/site-scripts.js
@@ -3,4 +3,20 @@ $(function() {
         e.preventDefault();
         $('#contact-form').submit();
     });
+
+    // Modal Logic
+
+    var $overlay = $('#overlay');
+
+    $(".expert-modal").click(function() {
+    	$(this).parent().find(".b-modal").addClass("m-active");
+        $overlay.addClass('m-active');
+    });
+
+    $(".expert-close-modal").click(function() {
+        $(".b-modal").removeClass("m-active");
+        $overlay.removeClass('m-active');
+    });
+    
 });
+
